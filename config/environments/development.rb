468751af0c3f2d775137183bd4ac17988af794ff
mrks_dev <mrks.heumann@gmail.com>
2024-10-30 22:56:20 +0100

    feat: make devise email confirmation work
    feat: make devise nice

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,14 +31,29 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :cloudflare
 
-  # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  # Change this to true to see delivery errors
+  config.action_mailer.raise_delivery_errors = true
 
   # Make template changes take effect immediately.
   config.action_mailer.perform_caching = false
 
+  # Configure Devise to require confirmation in development
+  config.require_confirmation = true
+
   # Set localhost to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.default_url_options = { host: "localhost", port: 5100 }
+  config.action_mailer.default_options = { from: 'confirmation@bentobook.app' }
+  config.action_mailer.delivery_method = :smtp
+
+  # Specify outgoing SMTP server. Remember to add smtp/* credentials via rails credentials:edit.
+  config.action_mailer.smtp_settings = {
+    user_name: Rails.application.credentials.dig(:ses_smtp_user),
+    password: Rails.application.credentials.dig(:ses_smtp_password),
+    address: "email-smtp.eu-north-1.amazonaws.com",
+    port: 587,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -71,5 +86,5 @@ Rails.application.configure do
   # config.generators.apply_rubocop_autocorrect_after_generate!
 
   # Prevent emails from being sent in development
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
 end
