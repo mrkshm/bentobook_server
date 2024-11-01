d2b70d38da9f4989eac7f09fb80b9eb84aad040a
mrks_dev <mrks.heumann@gmail.com>
2024-11-01 12:36:54 +0100

    feat: add translations for cuisine types

--- app/models/cuisine_type.rb
+++ app/models/cuisine_type.rb
@@ -3,5 +3,10 @@ class CuisineType < ApplicationRecord
 
     validates :name, presence: true, uniqueness: {case_sensitive: false}
 
-    scope :alphabetical, -> { order(:name) }
+    # Use translated names for all alphabetical sorting
+    scope :alphabetical, -> { all.sort_by { |ct| ct.translated_name.downcase } }
+
+    def translated_name
+        I18n.t("cuisine_types.#{name}", default: name)
+    end
 end
