2f8d649f0eb5a5c6abaae26ba33ef7c9e950c53e
mrks_dev <mrks.heumann@gmail.com>
2024-12-12 15:36:54 +0100

    fix: fix deploy

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -312,7 +312,7 @@ Devise.setup do |config|
   # config.sign_in_after_change_password = true
 
   config.jwt do |jwt|
-    jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+    jwt.secret = ENV.fetch('DEVISE_JWT_SECRET_KEY') { Rails.application.credentials.devise_jwt_secret_key! }
 
     # Configure which requests should generate a token
     jwt.dispatch_requests = [
