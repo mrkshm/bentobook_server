85d99a120aa2a0ad5edc412c89fde1f6f347b5aa
mrks_dev <93521410+mrkshm@users.noreply.github.com>
2025-01-24 11:35:06 +0100

    101 search and order fixes (#102)
    * feat: implement full text search
    
    * feat: implement sort by distance

--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -3,29 +3,29 @@ class VisitsController < ApplicationController
     before_action :authenticate_user!
     before_action :set_visit, only: [:show, :edit, :update, :destroy]
     before_action :ensure_valid_restaurant, only: [:create, :update]
-  
+
     def index
       @pagy, @visits = pagy(current_user.visits.includes(:restaurant, :contacts, images: { file_attachment: :blob }))
     end
-  
+
     def show
       # This action will render the show view
     end
-  
+
     def new
       @visit = Visit.new
       @visit.restaurant_id = params[:restaurant_id] if params[:restaurant_id].present?
     end
-  
+
     def create
       @visit = current_user.visits.build(visit_params)
       save_visit(:new)
     end
-  
+
     def edit
         # @visit is already set by set_visit
     end
-  
+
     def update
       if @visit.update(visit_params)
         save_visit(:edit)
@@ -33,29 +33,29 @@ class VisitsController < ApplicationController
         render :edit, status: :unprocessable_entity
       end
     end
-  
+
     def destroy
       @visit.destroy
       redirect_to visits_path, notice: I18n.t('notices.visits.deleted')
     end
-  
+
     private
-  
+
     def set_visit
       @visit = current_user.visits.find(params[:id])
       rescue ActiveRecord::RecordNotFound
         flash[:alert] = I18n.t('errors.visits.not_found')
         redirect_to visits_path
     end
-  
+
     def visit_params
       params.require(:visit).permit(
-        :date, 
-        :title, 
-        :notes, 
-        :restaurant_id, 
-        :rating, 
-        :price_paid, 
+        :date,
+        :title,
+        :notes,
+        :restaurant_id,
+        :rating,
+        :price_paid,
         :price_paid_currency,
         contact_ids: []
       ).tap do |whitelisted|
@@ -64,11 +64,11 @@ class VisitsController < ApplicationController
         end
       end
     end
-  
+
     def valid_restaurant?(restaurant_id)
       current_user.restaurants.exists?(restaurant_id)
     end
-  
+
     def ensure_valid_restaurant
       return if params[:visit].blank? || params[:visit][:restaurant_id].blank?
       unless valid_restaurant?(params[:visit][:restaurant_id])
@@ -78,7 +78,7 @@ class VisitsController < ApplicationController
         render :new, status: :unprocessable_entity
       end
     end
-  
+
     def save_visit(render_action)
       if @visit.restaurant_id.blank?
         flash.now[:alert] = I18n.t('errors.visits.restaurant_required')
