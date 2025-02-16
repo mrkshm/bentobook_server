79717ca47b40720d460074c2edf2a1584ee3ce9e
mrks_dev <93521410+mrkshm@users.noreply.github.com>
2025-02-16 10:45:56 +0100

    chore: update to latest packages (#108)

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 # syntax=docker/dockerfile:1
 # check=error=true
 
-ARG RUBY_VERSION=3.3.4
+ARG RUBY_VERSION=3.4.1
 FROM docker.io/library/ruby:$RUBY_VERSION-slim AS base
 
 WORKDIR /rails
--- vite.config.ts
+++ vite.config.ts
@@ -1,34 +1,13 @@
-import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
-import tailwindcss from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
+import { defineConfig } from 'vite';
+import RubyPlugin from 'vite-plugin-ruby';
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  plugins: [
-    RubyPlugin(),
-  ],
+  plugins: [tailwindcss(), RubyPlugin()],
   resolve: {
     alias: {
       '@hotwired/stimulus': '@hotwired/stimulus/dist/stimulus.js',
       '@hotwired/turbo': '@hotwired/turbo/dist/turbo.es2017-esm.js',
     },
   },
-  css: {
-    postcss: {
-      plugins: [
-        tailwindcss,
-        autoprefixer,
-      ],
-    },
-  },
-  build: {
-    outDir: 'public/vite',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        application: 'app/frontend/entrypoints/application.js',
-        styles: 'app/frontend/stylesheets/application.css'
-      }
-    },
-  },
-})
+});
