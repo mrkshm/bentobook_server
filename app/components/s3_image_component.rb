f9b98e705911b687d3fec8f65fcb9fbb7b1912ff
mrks_dev <mrks.heumann@gmail.com>
2025-02-20 16:35:33 +0100

    wip: image handling

--- app/components/s3_image_component.rb
+++ app/components/s3_image_component.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class S3ImageComponent < ViewComponent::Base
-  attr_reader :image, :size, :html_class, :data
-
   def initialize(image:, size: :medium, html_class: nil, data: {})
     @image = image
     @size = size
@@ -11,60 +9,45 @@ class S3ImageComponent < ViewComponent::Base
   end
 
   def call
-    return unless @image&.attached?
+    return unless valid_image?
 
-    if @size == :original
-      image_tag(@image, **html_options)
-    else
-      image_tag(@image.variant(variant_key), **html_options)
-    end
+    image_tag(
+      image_source,
+      class: html_class,
+      data: @data,
+      loading: "lazy",
+      decoding: "async"
+    )
   end
 
   private
 
-  # Define variant options for different sizes
-  def variant_key
-    case @size
-    when :thumbnail
-      {
-        resize_to_fill: [100, 100],
-        format: :webp,
-        saver: { quality: 80 }
-      }
-    when :small
-      {
-        resize_to_limit: [300, 200],
-        format: :webp,
-        saver: { quality: 80 }
-      }
-    when :medium
-      {
-        resize_to_limit: [600, 400],
-        format: :webp,
-        saver: { quality: 80 }
-      }
-    when :large
-      {
-        resize_to_limit: [1200, 800],
-        format: :webp,
-        saver: { quality: 80 }
-      }
-    else
-      {}
-    end
+  def valid_image?
+    @image.is_a?(ActiveStorage::Attached) ||
+    @image.is_a?(ActiveStorage::VariantWithRecord) ||
+    @image.is_a?(ActiveStorage::Variant)
+  end
+
+  def image_source
+    @image.is_a?(ActiveStorage::Attached) ? @image.variant(variant_options) : @image
   end
 
   def html_class
-    ["object-cover", @html_class].compact.join(" ")
+    [ "object-cover", @html_class ].compact.join(" ")
   end
 
-  def html_options
-    {
-      class: html_class,
-      alt: "Image",
-      loading: "lazy",
-      data: @data,
-      decoding: "async"
-    }
+  def variant_options
+    case @size
+    when :thumbnail
+      { resize_to_fill: [ 100, 100 ], format: :jpg }
+    when :small
+      { resize_to_limit: [ 300, 200 ], format: :jpg }
+    when :medium
+      { resize_to_limit: [ 600, 400 ], format: :jpg }
+    when :large
+      { resize_to_limit: [ 1200, 800 ], format: :jpg }
+    else
+      {}
+    end
   end
 end
