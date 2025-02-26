5485874952bd10aa987bfa0db2ac8bf23d2b3fa0
mrks_dev <93521410+mrkshm@users.noreply.github.com>
2025-02-26 12:17:37 +0100

    121 rip out alpinejs (#123)
    * wip: put places autocomplete in its own turbo frame
    * feat: populate form
    * feat: remove alpinejs
    * fix: fix forms

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -60,6 +60,71 @@ class RestaurantsController < ApplicationController
     @restaurant.cuisine_type = CuisineType.find_by(name: "other")
   end
 
+  def new_form
+    @restaurant = current_user.restaurants.new
+    @restaurant.build_google_restaurant
+    @cuisine_types = CuisineType.all
+    @restaurant.cuisine_type = CuisineType.find_by(name: "other")
+
+    if params[:place].present?
+      place_params = params.require(:place).permit(
+        :google_place_id, :name, :latitude, :longitude,
+        :formatted_address, :phone_number, :website, :rating,
+        :user_ratings_total, :business_status, :street_number,
+        :street_name, :city, :state, :postal_code, :country,
+        :price_level
+      )
+
+      # Convert Google's rating (float) to our rating (integer)
+      converted_rating = place_params[:rating].present? ? place_params[:rating].to_f.round : nil
+
+      @restaurant.assign_attributes(
+        name: place_params[:name],
+        street_number: place_params[:street_number],
+        street: place_params[:street_name],
+        city: place_params[:city],
+        state: place_params[:state],
+        postal_code: place_params[:postal_code],
+        country: place_params[:country],
+        phone_number: place_params[:phone_number],
+        business_status: place_params[:business_status],
+        latitude: place_params[:latitude],
+        longitude: place_params[:longitude],
+        rating: converted_rating,
+        price_level: place_params[:price_level]  # Google's price_level is already 1-4
+      )
+
+      @restaurant.google_restaurant.assign_attributes(
+        google_place_id: place_params[:google_place_id],
+        name: place_params[:name],
+        latitude: place_params[:latitude],
+        longitude: place_params[:longitude],
+        street_number: place_params[:street_number],
+        street: place_params[:street_name],
+        city: place_params[:city],
+        state: place_params[:state],
+        postal_code: place_params[:postal_code],
+        country: place_params[:country],
+        phone_number: place_params[:phone_number],
+        google_rating: place_params[:rating],
+        google_ratings_total: place_params[:user_ratings_total],
+        price_level: place_params[:price_level],
+        business_status: place_params[:business_status],
+        google_updated_at: Time.current
+      )
+    end
+
+    respond_to do |format|
+      format.turbo_stream {
+        render turbo_stream: turbo_stream.update(
+          "restaurant_form",
+          partial: "form",
+          locals: { restaurant: @restaurant }
+        )
+      }
+    end
+  end
+
   def update
     if @restaurant.update(restaurant_params)
       respond_to do |format|
@@ -191,6 +256,13 @@ class RestaurantsController < ApplicationController
       end
     else
       redirect_to restaurant_path(@restaurant), alert: "No tag provided."
+    end
+  end
+
+  def search
+    respond_to do |format|
+      format.html { render partial: "search_form" }
+      format.turbo_stream
     end
   end
 
