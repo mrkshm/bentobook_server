22c04a154171ffc91b998715cd4b7366f6d1a5d4
mrks_dev <mrks.heumann@gmail.com>
2025-03-19 09:43:50 +0100

    wip: nice titling

--- app/components/visits/title_component.rb
+++ app/components/visits/title_component.rb
@@ -4,14 +4,25 @@ module Visits
     include ActionView::RecordIdentifier
     include Rails.application.routes.url_helpers
 
-    def initialize(visit:)
+    def initialize(visit:, primary: true)
       @visit = visit
+      @primary = primary
     end
 
     private
 
+    attr_reader :visit, :primary
+
     def frame_id
-      dom_id(@visit, :title)
+      dom_id(visit, :title)
+    end
+
+    def link_classes
+      if primary
+        "block text-2xl font-medium text-surface-900 hover:text-primary-600 transition-colors"
+      else
+        "block text-surface-500 hover:text-primary-600 transition-colors"
+      end
     end
   end
 end
