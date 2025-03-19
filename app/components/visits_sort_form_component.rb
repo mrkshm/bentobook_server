da7617317b0e97f7e5de10796b550e898fbdaba0
mrks_dev <mrks.heumann@gmail.com>
2025-03-19 14:17:05 +0100

    feat: visit sorting

---
+++ app/components/visits_sort_form_component.rb
@@ -0,0 +1,11 @@
+class VisitsSortFormComponent < ApplicationComponent
+  include HeroiconHelper
+
+  def initialize(url:, fields:, current_order:, current_direction:, additional_fields: {})
+    @url = url
+    @fields = fields
+    @current_order = current_order
+    @current_direction = current_direction
+    @additional_fields = additional_fields
+  end
+end
--- app/controllers/visits_controller.rb
+++ app/controllers/visits_controller.rb
@@ -5,7 +5,35 @@ class VisitsController < ApplicationController
     before_action :ensure_valid_restaurant, only: [ :create, :update ]
 
     def index
-      @pagy, @visits = pagy(current_user.visits.includes(:restaurant, { contacts: { avatar_attachment: :blob } }, images: { file_attachment: :blob }))
+      @order_by = params[:order_by] || "date"
+      @order_direction = %w[asc desc].include?(params[:order_direction]) ? params[:order_direction] : "desc"
+      @search = params[:search]
+
+      visits = current_user.visits
+        .includes(:restaurant, { contacts: { avatar_attachment: :blob } }, images: { file_attachment: :blob })
+
+      visits = visits.search_by_full_text(@search) if @search.present?
+
+      visits = case @order_by
+      when "date"
+                visits.order(Arel.sql("date #{@order_direction}"))
+      when "restaurant_name"
+                visits.joins(:restaurant)
+                     .order(Arel.sql("LOWER(restaurants.name) #{@order_direction}"))
+      when "rating"
+                visits.order(Arel.sql("rating #{@order_direction} NULLS LAST"))
+      when "created_at"
+                visits.order(Arel.sql("created_at #{@order_direction}"))
+      else
+                visits.order(Arel.sql("date DESC"))
+      end
+
+      @pagy, @visits = pagy(visits)
+
+      respond_to do |format|
+        format.html
+        format.turbo_stream if params[:page]
+      end
     end
 
     def show
