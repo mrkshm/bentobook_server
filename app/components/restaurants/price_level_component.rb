40932b6023dcdfd1bcacd77dcfdb2fc842651653
mrks_dev <mrks.heumann@gmail.com>
2025-04-08 14:01:34 +0200

    refactor: restaurants price level component

--- app/components/restaurants/price_level_component.rb
+++ app/components/restaurants/price_level_component.rb
@@ -2,9 +2,21 @@
 
 module Restaurants
   class PriceLevelComponent < ApplicationComponent
+    include HeroiconHelper
+
     def initialize(restaurant:, readonly: false)
       @restaurant = restaurant
       @readonly = readonly
+    end
+
+    def frame_id
+      "restaurant_#{restaurant.id}_price_level"
+    end
+
+    def hotwire_native_app?
+      helpers.hotwire_native_app?
+    rescue NoMethodError
+      false
     end
 
     private
