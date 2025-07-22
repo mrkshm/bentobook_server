6909cde81538f548743d60f16cfd9ff344aee75d
mrks_dev <mrks.heumann@gmail.com>
2025-07-22 11:49:49 +0200

    feat: redirect on launch

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,11 @@ class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home, :terms]
 
   def home
+    if user_signed_in?
+      redirect_to home_dashboard_path
+      return
+    end
+    
     # The variant will be automatically set by ApplicationController
     respond_to do |format|
       format.html
